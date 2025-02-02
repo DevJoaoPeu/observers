3c083f0eae2b62a8cccd1776b9bc112f5c7a86d7
DevJoaoPeu <joaocanaldroid@gmail.com>
2025-02-02 19:25:57 -0300

    feat: criando dois components e 1 serviço

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SenderMessageComponent } from './sender-message/sender-message.component';
+import { ReceiveMessageComponent } from './receive-message/receive-message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SenderMessageComponent,
+    ReceiveMessageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/sender-message/sender-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sender-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './sender-message.component.html',
+  styleUrl: './sender-message.component.scss'
+})
+export class SenderMessageComponent {
+
+}
